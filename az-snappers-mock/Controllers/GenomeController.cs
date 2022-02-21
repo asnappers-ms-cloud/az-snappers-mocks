26defcb2954e3a61ef83dd173b5127df1e17d8d9
Shashi <37266337+shashiky@users.noreply.github.com>
2022-02-21 15:21:22 -0500

    Create GenomeController.cs

---
+++ az-snappers-mock/Controllers/GenomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace az_snappers_mock.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class GenomeController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Genome test service";
+        }
+    }
+}
